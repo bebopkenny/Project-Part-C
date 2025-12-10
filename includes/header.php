239653c6dd7cf297fada8c3028728be4c375d0fb
MedrianB <112015049+MedrianB@users.noreply.github.com>
2025-12-10 12:37:38 -0800

    Update header.php

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<?php function theatre_header() { 
+
+	return '
+   <header id = "header-style">
+       <nav>
+            <a href="index.php">Homepage</a>
+            <a href="movies.php">Movies</a>
+            <a href="showtimes.php">Movie Showtimes</a>
+            <a href="my_tickets.php">Tickets</a>
+            <a href="reports.php">Reports</a>
+       </nav>
+  </header>
+	';
+} ?>
